50958ee882b121c29e82e8d1affac4f2477940f3
Prem Nirmal <prem.nirmal88@gmail.com>
2013-02-15 15:06:57 -0500

    Minor bug fixes

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,14 +2,17 @@ class Address < ActiveRecord::Base
   belongs_to :entry
   attr_accessible :city, :state, :street, :zip, :entry_id
   validates :city, :state, :street, :zip, :presence => true
-  validates :state, :length => { :minimum => 2 }, :format => {
+  validates :state, :format => {
   	:with => /^(?:A[KLRZ]|C[AOT]|D[CE]|FL|GA|HI|I[ADLN]|K[SY]|LA|M[ADEINOST]|N[CDEHJMVY]|O[HKR]|PA|RI|S[CD]|T[NX]|UT|V[AT]|W[AIVY])*$/,
-    :message => "Only two letters for valid US state allowed"
+    :message => "PLEASE ENTER A VALID STATE"
   }
-  # format zip code w/ leading zeroes
-  # def zip
-  # 	"%05d" % read_attributes(:zip)
-  # end
+  validates_length_of :state, :in => 2..2, :message => "Must be two characters only"
+  validates_length_of :zip, :in => 0..5, :message => "MAX OF 5 DIGITS FOR ZIP"
+  #validates :zip :length => { :minimum => 0, :maximum => 5}
+  #format zip code w/ leading zeroes
+  def zip
+  	"%05d" % read_attribute(:zip)
+  end
 
   def state=(val)
   	write_attribute(:state, val.upcase)
