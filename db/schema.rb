f03cad45abcf5cb7bff1dc1900fbd1e4d9675dfd
Prem Nirmal <prem.nirmal88@gmail.com>
2013-02-25 21:19:55 -0500

    Modified seed so that AddressType ID is consistent Modified stylesheet a little

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(:version => 20130212013948) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
