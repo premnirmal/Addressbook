8d2a7fcaac942f843636a7246455a994c2d2e151
Prem Nirmal <prem.nirmal88@gmail.com>
2013-04-19 20:38:51 -0400

    Added alphabetical tabs

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,15 +3,24 @@ class EntriesController < ApplicationController
   # GET /entries.json
   respond_to :html
   before_filter :get_entries
+  before_filter :find_entry, :only => [:show, :edit, :destroy, :update]
+  
+  def find_entry
+    @entry = Entry.find(params[:id])
+    params[:initial] = @entry.last_name[0]
+  end
 
   def get_entries
-    @entries = Entry.find(:all,:order => 'last_name')
+    @entries = 
+      Entry.where('substr(last_name,1,1)= ?',
+                  params[:initial] || 'A').order('last_name, first_name')
     @feedbacks = Feedback.find(:all, :order => 'id DESC')
     @feedback = Feedback.new
   end
-
+  
   def index
-   respond_with (@entries = Entry.find(:all,:order => 'last_name'))
+    respond_with (@entries = Entry.where('substr(last_name,1,1)= ?',
+                                         params[:initial] || 'A').order('last_name, first_name'))
   end
 
   # GET /entries/1
@@ -33,7 +42,7 @@ class EntriesController < ApplicationController
   def edit
     @entry = Entry.find(params[:id])
   end
-
+  
   # POST /entries
   # POST /entries.json
   def create
@@ -56,7 +65,7 @@ class EntriesController < ApplicationController
       end
     end
   end
-
+  
   # DELETE /entries/1
   # DELETE /entries/1.json
   def destroy
