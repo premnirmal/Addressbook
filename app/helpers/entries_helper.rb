659def8cba8c5dff360ed78bc2dacf5f2a54c0ce
Prem Nirmal <prem.nirmal88@gmail.com>
2013-04-26 23:16:44 -0400

    Rounded corners for images

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,6 +1,11 @@
 module EntriesHelper
   def avatar(entry,width)
-    cl_image_tag(entry.avatar.blank? ? 'default.jpg' : entry.avatar,
-                 width: width, crop: 'fill')
+    if width > 100
+      cl_image_tag(entry.avatar.blank? ? 'default.jpg' : entry.avatar,
+                   width: width, crop: 'fill', radius: 20)
+    else
+      cl_image_tag(entry.avatar.blank? ? 'default.jpg' : entry.avatar,
+                   width: width, crop: 'fill')
+    end
   end
 end
