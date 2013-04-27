a37e5fcc0cd695d65d3b7fec3dc391afbc35aa47
Prem Nirmal <prem.nirmal88@gmail.com>
2013-04-26 23:02:33 -0400

    Added images, yet to add image to forms

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -1,6 +1,6 @@
 module EntriesHelper
   def avatar(entry,width)
-    cl_image_tag(@entry.avatar.blank? ? 'default.jpg' : @entry.avatar,
+    cl_image_tag(entry.avatar.blank? ? 'default.jpg' : entry.avatar,
                  width: width, crop: 'fill')
   end
 end
